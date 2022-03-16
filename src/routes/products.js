302956018538172a8760440afd9761986236b72e
unknown <sebmar92@gmail.com>
2022-03-15 22:55:46 -0300

    can search by query

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,8 +3,16 @@ const { Product } = require("../database.js");
 const getProducts = require("../controllers/products/getProducts.js");
 
 router.get("", async function (req, res) {
+  const { search } = req.query;
+  if (search) {
+    const allProducts = await getProducts();
+    const searchedProduct = allProducts.filter((product) => {
+      return product.name.toLowerCase().includes(search.toLowerCase());
+    });
+    return res.status(200).send(searchedProduct);
+  }
   const products = await getProducts();
-  res.send(products);
+  return res.status(200).send(products);
 });
 
 module.exports = router;
