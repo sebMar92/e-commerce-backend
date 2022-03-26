1014d6e99c48095ec80aa8ca4a43e835328ac657
unknown <sebmar92@gmail.com>
2022-03-26 13:57:32 -0300

    get started

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -1,23 +1,29 @@
-const router = require("express").Router();
-const authToken = require("./middlewares/authToken.js");
-const createAndAddSale = require("../controllers/sales/createAndAddSale.js");
-const deleteSale = require("../controllers/sales/deleteSale.js");
+const router = require('express').Router();
+const authToken = require('./middlewares/authToken.js');
+const createAndAddSale = require('../controllers/sales/createAndAddSale.js');
+const deleteSale = require('../controllers/sales/deleteSale.js');
+const getSales = require('../controllers/sales/getSales.js');
 
-router.post("", authToken, async function (req, res) {
+router.post('', authToken, async function (req, res) {
   const newSale = await createAndAddSale(req.body);
   if (newSale) {
-    return res.send({ msg: "sale created" });
+    return res.send({ msg: 'sale created' });
   }
   return res.send({ error: "couldn't create sale" });
 });
 
-router.delete("", authToken, async function (req, res) {
+router.delete('', authToken, async function (req, res) {
   const { id } = req.body;
   const didDelete = deleteSale(id);
   if (didDelete) {
-    return res.send({ msg: "sale deleted" });
+    return res.send({ msg: 'sale deleted' });
   }
   return res.send({ error: "couldn't delete sale" });
+});
+
+router.get('', async function (req, res) {
+  const sales = await getSales();
+  res.send(sales);
 });
 
 module.exports = router;
