f85f02b55e7fc0325e8942c513b2f3ea0a4b2e42
unknown <sebmar92@gmail.com>
2022-03-27 15:30:00 -0300

    order routes fixed

--- src/controllers/orders/changeOrderAmount.js
+++ src/controllers/orders/changeOrderAmount.js
@@ -1,14 +1,14 @@
-const { Order } = require("../../database.js");
+const { Order } = require('../../database.js');
 
 const changeOrderAmount = async (orderId, amount) => {
   const order = await Order.findOne({ where: { id: orderId } });
   if (order) {
-    if (order.amount + amount > 1) {
+    if (Number(order.amount) + Number(amount) < 1) {
       await Order.destroy({ where: { id: orderId } });
 
       return true;
     }
-    order.amount = order.amount + amount;
+    order.amount = Number(order.amount) + Number(amount);
     await order.save();
 
     return true;
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,22 +1,23 @@
-const router = require("express").Router();
-const changeOrderAmount = require("../controllers/orders/changeOrderAmount.js");
-const changeOrderStatus = require("../controllers/orders/changeOrderStatus.js");
-const createOrder = require("../controllers/orders/createOrder.js");
-const getProductsWithOrders = require("../controllers/orders/getProductsWithOrders.js");
-const authToken = require("./middlewares/authToken.js");
+const router = require('express').Router();
+const changeOrderAmount = require('../controllers/orders/changeOrderAmount.js');
+const changeOrderStatus = require('../controllers/orders/changeOrderStatus.js');
+const createOrder = require('../controllers/orders/createOrder.js');
+const deleteOrder = require('../controllers/orders/deleteOrder.js');
+const getProductsWithOrders = require('../controllers/orders/getProductsWithOrders.js');
+const authToken = require('./middlewares/authToken.js');
 //create order (depende el status si la agrega a wishlist, carrito, pasa a pendiente de compra -durante la pasarela- o a compra terminada)
-router.post("", authToken, async function (req, res) {
+router.post('', authToken, async function (req, res) {
   const { status, amount, productId } = req.body;
   const user = req.user.user;
 
   const created = await createOrder(status, amount, user, productId);
   if (created) {
-    return res.send({ msg: "order created" });
+    return res.send({ msg: 'order created' });
   }
   return res.send({ error: "couldn't create order" });
 });
 // trae las ordenes con el status pedido (para traer el carro de compras, la wishlist, historial, etc)
-router.get("", authToken, async function (req, res) {
+router.get('', authToken, async function (req, res) {
   const user = req.user.user;
   const { status } = req.query;
 
@@ -28,30 +29,30 @@ router.get("", authToken, async function (req, res) {
 });
 //modificar estado de orden (pasar de wishlist a carrito, de carrito a pendiente, de pendiente a terminado, etc)
 //modificar cantidad de la orden
-router.put("/:id", authToken, async function (req, res) {
-  const { status, amount } = req.body;
-  const { id } = req.query;
+router.put('/:id', authToken, async function (req, res) {
+  const { status, amount } = req.query;
+  const { id } = req.params;
   if (status) {
     const orderChanged = await changeOrderStatus(id, status);
     if (orderChanged) {
-      return res.send({ msg: "status changed" });
+      return res.send({ msg: 'status changed' });
     }
   } else if (amount) {
     const orderChanged = await changeOrderAmount(id, amount);
     if (orderChanged) {
-      return res.send({ msg: "amount changed" });
+      return res.send({ msg: 'amount changed' });
     }
   }
 
   return res.send({ error: "couldn't edit order" });
 });
 // borrar order (quitar de wishlist, quitar de carrito, cancelar compra)
-router.delete("/:id", authToken, async function (req, res) {
-  const { id } = req.query;
+router.delete('/:id', authToken, async function (req, res) {
+  const { id } = req.params;
 
-  const orderDeleted = await changeOrderStatus(id);
+  const orderDeleted = await deleteOrder(id);
   if (orderDeleted) {
-    return res.send({ msg: "order deleted" });
+    return res.send({ msg: 'order deleted' });
   }
   return res.send({ error: "couldn't find order" });
 });
