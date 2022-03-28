1e57d673c16b0f7d2f9ad0782b052e57fbd0c449
unknown <sebmar92@gmail.com>
2022-03-27 22:05:42 -0300

    admin checks

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,14 @@
-const router = require("express").Router();
-const getProducts = require("../controllers/products/getProducts.js");
-const countPages = require("../controllers/products/utils/countPages.js");
-const filterCompose = require("../controllers/products/utils/filterCompose.js");
-const orderCompose = require("../controllers/products/utils/orderCompose.js");
-const paginationCompose = require("../controllers/products/utils/paginationCompose.js");
-const { Sale } = require("../database.js");
-const createProduct = require("../controllers/products/createProduct.js");
+const router = require('express').Router();
+const getProducts = require('../controllers/products/getProducts.js');
+const countPages = require('../controllers/products/utils/countPages.js');
+const filterCompose = require('../controllers/products/utils/filterCompose.js');
+const orderCompose = require('../controllers/products/utils/orderCompose.js');
+const paginationCompose = require('../controllers/products/utils/paginationCompose.js');
+const { Sale } = require('../database.js');
+const createProduct = require('../controllers/products/createProduct.js');
+const authToken = require('./middlewares/authToken.js');
 
-router.get("", async function (req, res) {
+router.get('', async function (req, res) {
   const { search, minPrice, maxPrice, freeShipping, categoryId, order, limit, offset } =
     req.query;
   const filterConditions = await filterCompose(
@@ -27,7 +28,7 @@ router.get("", async function (req, res) {
   const pages = await countPages(limit, ...filterConditions);
   const globalSales = await Sale.findAll({
     where: { global: true },
-    attributes: ["percentage", "day", "productAmount", "id"],
+    attributes: ['percentage', 'day', 'productAmount', 'id'],
   });
   return res.status(200).send({
     products: products,
@@ -37,11 +38,13 @@ router.get("", async function (req, res) {
   });
 });
 
-router.post("/", async (req, res)=>{
-  
-  const creado = await createProduct(req.body)
-  res.send(creado);
-})
-
+router.post('/', authToken, async (req, res) => {
+  if (req.user.user.rol === 'admin') {
+    const creado = await createProduct(req.body);
+    return res.send(creado);
+  } else {
+    return res.status(403).send({ error: "You don't have permision to do this." });
+  }
+});
 
 module.exports = router;
--- src/routes/sales.js
+++ src/routes/sales.js
@@ -6,28 +6,40 @@ const getSales = require('../controllers/sales/getSales.js');
 const editSale = require('../controllers/sales/editSale.js');
 
 router.post('', authToken, async function (req, res) {
-  const newSale = await createAndAddSale(req.body);
-  if (newSale) {
-    return res.send({ msg: 'sale created' });
+  if (req.user.user.rol === 'admin') {
+    const newSale = await createAndAddSale(req.body);
+    if (newSale) {
+      return res.send({ msg: 'sale created' });
+    }
+    return res.send({ error: "couldn't create sale" });
+  } else {
+    return res.status(403).send({ error: "You don't have permision to do this." });
   }
-  return res.send({ error: "couldn't create sale" });
 });
 
 router.put('', authToken, async function (req, res) {
-  const editedSale = await editSale(req.body);
-  if (editedSale) {
-    return res.send({ msg: 'sale edited' });
+  if (req.user.user.rol === 'admin') {
+    const editedSale = await editSale(req.body);
+    if (editedSale) {
+      return res.send({ msg: 'sale edited' });
+    }
+    return res.send({ error: "couldn't edit sale" });
+  } else {
+    return res.status(403).send({ error: "You don't have permision to do this." });
   }
-  return res.send({ error: "couldn't edit sale" });
 });
 
 router.delete('', authToken, async function (req, res) {
-  const { saleId } = req.query;
-  const didDelete = deleteSale(saleId);
-  if (didDelete) {
-    return res.send({ msg: 'sale deleted' });
+  if (req.user.user.rol === 'admin') {
+    const { saleId } = req.query;
+    const didDelete = deleteSale(saleId);
+    if (didDelete) {
+      return res.send({ msg: 'sale deleted' });
+    }
+    return res.send({ error: "couldn't delete sale" });
+  } else {
+    return res.status(403).send({ error: "You don't have permision to do this." });
   }
-  return res.send({ error: "couldn't delete sale" });
 });
 
 router.get('', async function (req, res) {
