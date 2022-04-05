04557dbc2ff2172d0f026c5a0dcccb1a85a68536
FelipeCiroM <felipe.cirom@udea.edu.co>
2022-04-05 16:39:41 -0500

    sendEmails

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const { PASS } = process.env;
 
 router.post("", (req, res) => {
-  const { receivers, message, title } = req.body;
+  const { receivers, message, title, products, link, name, type } = req.body;
 
   var transporter = nodemailer.createTransport({
     service: "gmail",
@@ -13,12 +13,21 @@ router.post("", (req, res) => {
       pass: PASS,
     },
   });
-
-  var mailOptions = {
-    from: "techstore421@gmail.com",
-    to: `${receivers}`,
-    subject: `${title}`,
-    html: `
+  let arr = [];
+  products.map((i) => {
+    return arr.push({
+      title: i.title,
+      price: i.price,
+      images: i.images[0].url,
+    });
+  });
+  let mailOptions = {};
+  if (type === "purchase") {
+    mailOptions = {
+      from: "techstore421@gmail.com",
+      to: `${receivers}`,
+      subject: `${title}`,
+      html: `
     <html lang="en">
     <head>
       <meta charset="UTF-8">
@@ -27,11 +36,40 @@ router.post("", (req, res) => {
       <title>Techstore</title>
     </head>
     <body>
-      <h1>${message}</h1>
+      <h1>Hello ${name}!<h1/>
+      <h3>${message}</h3>
+      <h4>Thank you for placing your order with TechStore!<h4/>
+      <h4>We really appreciate that you chose our store, it means the world to us!<h4/>
+      <h4>We hope you enjoy it.<h4/>
+      <h4>Have a great day!<h4/>
+      <br/>
+      <h2>These are the products you have just purchased:<h2/>
+      <h4>${arr.map((i) => i.title)}<h4/>
+      <h2>See details here: ${link}<h2/>
     </body>
     </html>
   `,
-  };
+    };
+  } else {
+    mailOptions = {
+      from: "techstore421@gmail.com",
+      to: `${receivers}`,
+      subject: `${title}`,
+      html: `
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+      <title>Techstore</title>
+    </head>
+    <body>
+      <h1>Hello ${name}!<h1/>
+    </body>
+    </html>
+  `,
+    };
+  }
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
